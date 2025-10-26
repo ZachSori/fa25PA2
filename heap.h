95c472f7a61848f04b3d3b40ac3d177aa521a80b
ZachSori <zsoriano0042@sdsu.edu>
2025-10-26 16:41:41 -0700

    implement up/down heaps

--- heap.h
+++ heap.h
@@ -18,7 +18,7 @@ struct MinHeap {
         // TODO: insert index at end of heap, restore order using upheap()
         if (size==64) return;
         data[size] = idx;
-        //upheap()
+        upheap(size, weightArr);
         size++;
     }
 
@@ -31,16 +31,37 @@ struct MinHeap {
         int min = data[0];
         size--;
         data[0] = data[size];
-        //downheap();
+        downheap(0, weightArr);
         return min;
     }
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
+        while (pos>0) {
+            int parent = (pos-1)/2;
+            if (weightArr[data[pos]] < weightArr[data[parent]]) {
+                swap(data[pos], data[parent]);
+                pos = parent;
+            }
+            else break;
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
+        while (pos*2+1<size) {
+            int LC = pos*2 + 1;
+            int RC = pos*2 + 2;
+            int min = LC;
+            //if RC exist && if RC<LC
+            if (RC<size && weightArr[data[RC]]<weightArr[data[LC]]) min = RC;
+            if (weightArr[data[pos]] < weightArr[data[min]]) {
+                swap(data[pos], data[min]);
+                pos = min;
+            }
+            else break;
+
+        }
     }
 };
 
